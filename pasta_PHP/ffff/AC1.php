e7b8d05f0e3770000859d593a7fc5388af57dbb3
joaovictorpimentelborba <joaovictorpimentel981@gmail.com>
2025-11-03 11:22:08 +0000

    sdf

---
+++ pasta_PHP/ffff/AC1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AC1</title>
+    <link rel="stylesheet" href="index1.css">
+</head>
+
+<body>
+    <?php 
+    require "header.php";
+    require "footer.php";
+    ?>
+</body>
+
+</html>
---
+++ pasta_PHP/ffff/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="index1.css">
+</head>
+<body>
+    <header>
+        <h1 id="Titulo">H1 MUITO PIKA</h1>
+        <nav>
+            <a href="AC1.php">Assasin's creed</a>
+            <a href="AC2.php">Ezio Arc</a>
+            <a href="#">USA Arc</a>
+            <a href="#">Pirates Arc</a>
+        </nav>
+    </header>
+</body>
+</html>
