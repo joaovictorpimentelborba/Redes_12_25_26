4d78bd6685463aca630257859ee88b70ce892902
joaovictorpimentelborba <joaovictorpimentel981@gmail.com>
2026-01-28 19:05:05 +0000

    dad

---
+++ Recuperação de Modulo/Index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="saude.css">
+    <link rel="shortcut icon" href="imgs/logo.jpeg" type="image/x-icon">
+
+</head>
+
+<body>
+    <header>
+        <div><img src="imgs/logo.png" alt="" width="90px"></div>
+        <h1>FitLife ajuda a saber mais sobre ti</h1>
+    </header>
+    <Main>
+        <div class="carrocel">
+            <div class="group">
+                <button><img src="imgs/Calcular Gordura Corporal.png" alt="" class="item gordura"
+                        width="250px"></img></button>
+                <button><img src="imgs/Calcular IMC.png" alt="" class="item imc" width="250px"></img></button>
+                <button><img src="imgs/Calculadora de Calorias.png" alt="" class="item cal"
+                        width="250px"></img></button>
+            </div>
+            <div class="group">
+                <button><img src="imgs/Calcular Gordura Corporal.png" alt="" class="item gordura"
+                        width="250px"></img></button>
+                <button><img src="imgs/Calcular IMC.png" alt="" class="item imc" width="250px"></img></button>
+                <button><img src="imgs/Calculadora de Calorias.png" alt="" class="item" width="250px"></img></button>
+            </div>
+            <div class="group">
+                <button><img src="imgs/Calcular Gordura Corporal.png" alt="" class="item gordura"
+                        width="250px"></img></button>
+                <button><img src="imgs/Calcular IMC.png" alt="" class="item imc" width="250px"></img></button>
+                <button><img src="imgs/Calculadora de Calorias.png" alt="" class="item cal"
+                        width="250px"></img></button>
+            </div>
+        </div>
+    </Main>
+
+</body>
+
+</html>
+
+<?php 
+
+
+
+?>
