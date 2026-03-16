4ed85786e3e1dfef3d179d54c07c21e9427584e6
joaovictorpimentelborba <joaovictorpimentel981@gmail.com>
2026-03-16 13:00:40 +0000

    sd

---
+++ pasta_PHP/ficha 2 biblioteca/biblioteca .php
@@ -0,0 +1,44 @@
+<?php 
+$con = mysqli_connect('localhost','root','root','biblioteca',3306);
+
+if($con->connect_error){
+    die("Ocorreu um erro ao ligar a DB".$con->connect_error);
+}
+else{
+    echo"A ligar DB";
+}
+echo "<br>";
+
+$inserir1="insert into t_livro (titulo, data_emicao, autor, uso) values('Canaviais','2022/05/15', 'Gugu',0);";
+$inserir2="insert into t_livro (titulo, data_emicao, autor, uso) values('Santana','1945/07/23', 'Galvão Boeno',1);";
+
+if($con->query($inserir1)){
+    echo"Registro 1 inserido";
+}
+else {
+    echo"erro ao inserir 1". $con->error;
+}
+echo "<br>";
+if($con->query($inserir2)){
+    echo"Registro 2 inserido";
+}
+else {
+    echo"erro ao inserir 2".$con->error;
+}
+echo "<br>";
+$consulta1="select *from t_livro;";
+$resultado= $con->query($consulta1);
+
+
+if($resultado->num_rows>0){
+    while($registro=$resultado->fetch_assoc()){
+        echo "Titulo: ".$registro['titulo']." Data Emicao: ".$registro['data_emicao']." Autor: ".$registro['autor']." Uso: ".$registro['uso']."<br>";
+    }
+}
+else{
+    echo"nada encontrado!!!";
+}
+echo "<br>";
+
+
+?>
