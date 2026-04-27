23db0bf2fca8fc3959fdf1ffffee5314f6b976ea
joaovictorpimentelborba <joaovictorpimentel981@gmail.com>
2026-04-27 11:32:33 +0100

    hbjnm

--- pasta_PHP/Projtoto_Frutas/ligar.php
+++ pasta_PHP/Projtoto_Frutas/ligar.php
@@ -1,31 +1,11 @@
 <?php
 
-$servidor = "localhost";
-$user = "root";
-$pass = "";
+$con = mysqli_connect('localhost','root','root', '',3306);
 
-try {
+$sql = "CREATE DATABASE IF NOT EXISTS db_frutas";
+mysqli_query($con, $sql);
 
-    $conn = new PDO("mysql:host=$servidor", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $conn->exec("CREATE DATABASE IF NOT EXISTS db_Frutas");
-
-    $conn->exec("USE db_Frutas");
-
-    $conn->exec("
-    CREATE TABLE IF NOT EXISTS t_frutas(
-        id_fruta INT AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(100),
-        preco DECIMAL(10,2)
-    )");
-
-    // voltar ao index automaticamente
-    header("Location: index.php");
-    exit();
-
-} catch(PDOException $e){
-    echo "Erro: " . $e->getMessage();
-}
+header("Location: index.php");
+exit();
 
 ?>
